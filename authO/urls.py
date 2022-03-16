d8414302e5b7217b4ef9e60fe8ce7fdcafa2f6d6
markvangeffen <81081004+markvangeffen@users.noreply.github.com>
2022-03-16 10:19:38 +0100

    Auth0 working on website for login and logout.

---
+++ authO/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+# To disable Django's login screen:
+from django.contrib.auth.decorators import login_required
+from django.contrib import admin
+
+admin.autodiscover()
+admin.site.login = login_required(admin.site.login)
+
+urlpatterns = [ 
+    path('logout/', views.logout, name='logout'),
+]
+
